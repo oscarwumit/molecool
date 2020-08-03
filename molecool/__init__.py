e32943b371c488daad1786e219b95f0f14a50c23
Oscar Wu <oscarwu@mit.edu>
2020-08-03 14:39:26 -0400

    Initial commit after CMS Cookiecutter creation, version 1.3

---
+++ molecool/__init__.py
@@ -0,0 +1,14 @@
+"""
+molecool
+A Python package for analyzing and visualizing molecular structures. For NSF bootcamp.
+"""
+
+# Add imports here
+from .functions import *
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
